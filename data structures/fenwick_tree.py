d2b5efdd5c28c5223d149508859301a9f5b68a1e
Anton Sidarevich <sidorevich.anton@gmail.com>
2024-10-01 21:25:19 +0200

    Minor fixes

--- data structures/fenwick_tree.py
+++ data structures/fenwick_tree.py
@@ -1,24 +1,28 @@
 class FenwickTree:
     def __init__(self, arr: list[int]):
-        self.size = len(arr)
-        self.arr = arr
-        self.tree = [0] * self.size
+        self._size = len(arr)
+        self._arr = arr
+        self._tree = [0] * self._size
         self._build(arr)
+
+    @property
+    def size(self) -> int:
+        return self._size
 
     def _build(self, arr: list[int]):
         for index in range(len(arr)):
             self._inc(index, arr[index])
 
     def _inc(self, index: int, delta: int):
-        while index < self.size:
-            self.tree[index] += delta
+        while index < self._size:
+            self._tree[index] += delta
             index |= index + 1
 
     def _get_prefix_sum(self, index: int) -> int:
         result = 0
 
         while index > -1:
-            result += self.tree[index]
+            result += self._tree[index]
             index = (index & (index + 1)) - 1
 
         return result
@@ -27,8 +31,8 @@ class FenwickTree:
         return self._get_prefix_sum(r) - self._get_prefix_sum(l - 1)
 
     def update(self, index: int, value: int):
-        self._inc(index, value - self.arr[index])
-        self.arr[index] = value
+        self._inc(index, value - self._arr[index])
+        self._arr[index] = value
 
 
 def main():
